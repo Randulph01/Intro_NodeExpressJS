1e9d8bb8f05dea7644a1565482989e39feee22f7
YY221@ccs.local <YY221@ccs.local>
2025-02-05 05:20:39 -0800

    Second Commit

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
+app.get('/about', (req, res) => {
+    res.send('About Us');
+})
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
@@ -9,3 +14,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+app.use(express.json());
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
+})

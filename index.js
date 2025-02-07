fd4f8944759b94665567124a49c0e39d77cde65a
unknown <MM521@ccs.local>
2025-02-07 12:40:42 +0800

    Updated repository

--- index.js
+++ index.js
@@ -21,3 +21,31 @@ app.post('/submit', (req, res) => {
     const data = req.body;
     res.send(`Received: ${JSON.stringify(data)}`);
 })
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+app.use(express.json()); //Middleware
+
+app.use((req, res, next) => { //Logging Middleware
+    console.log(`${req.method} ${req.url}`);
+    next();
+})
+
+app.use((err, req, res, next) => { //Error Handling
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+const item = ['Apple', 'Banana', 'Orange'];
+
+app.get('/items', (req, res) => {
+    res.json(item);
+});
+
+app.post('/items', (req, res) => {
+    const data = req.body.item;
+    items.push(newItem);
+    res.json(items);
+});
